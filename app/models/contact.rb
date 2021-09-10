f3bd49e99b325deb84c899a6b0aef7d8c842351c
Ilan Ben Hagai <supox0@walla.co.il>
2021-09-10 17:46:58 +0300

    wip

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < MailForm::Base
   attribute :name,      validate: true
   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :phone,     phone: true
   attribute :message
   attribute :nickname,  captcha: true
 
