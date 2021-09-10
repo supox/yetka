07d8bfa92ba384d3b248ad70dedf8d5d9f239b10
Ilan Ben Hagai <supox0@walla.co.il>
2021-09-10 23:16:35 +0300

    wip

--- app/javascript/packs/gallery.js
+++ app/javascript/packs/gallery.js
@@ -17,12 +17,17 @@ $(function() {
     }
 
     function setMainImage(src) {
+        if (src === undefined) {
+            return;
+        }
         img.forEach(img => (img.style.opacity = 1));
         current.src = src;
         current.classList.add('fade-in');
         setTimeout(() => current.classList.remove('fade-in'), 200);
-        var index = imagesList.map(x => x.src).indexOf(current.src);
-        img[index].style.opacity = opacity;
+        var index = imagesList.map(x => x.src).indexOf(src);
+        if (index >= 0) {
+            img[index].style.opacity = opacity;
+        }
     }
 
     imgs.addEventListener('click', imgClick);
