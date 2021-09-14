56204a598b2bf8b7346ade04550d2b4b040ed7f3
Ilan Ben Hagai <supox0@walla.co.il>
2021-09-14 21:56:26 +0300

    mailgun: updated api host for EU

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,16 +122,7 @@ Rails.application.configure do
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MAILGUN_API_KEY'],
     domain: "mg.sheketbeanan.com",
-  }
-
-  # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings= {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
-    :authentication => :plain,
+    api_host: 'api.eu.mailgun.net',
   }
 
 end
