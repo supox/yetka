1acb3ce2bad382c829ab9e402774a2b6363c8a04
Ilan Ben Hagai <supox0@walla.co.il>
2021-09-17 16:50:48 +0300

    navbar stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def nav_link_to(name, href, cls="nav-link", active_class="active")
+        link_to_unless_current(name, href, class: cls) {|name| link_to(name, href, class: "#{cls} #{active_class}")}
+    end
 end
